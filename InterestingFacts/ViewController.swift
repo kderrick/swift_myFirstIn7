0259b002f4aa5743e457073b3bacfb1ca9427c66
Kyle <kderrick1988@gmail.com>
2016-10-04 07:55:13 -0700

    Initial Commit

---
+++ InterestingFacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InterestingFacts
+//
+//  Created by Kyle Derrick on 10/4/16.
+//  Copyright © 2016 Kyle Derrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
